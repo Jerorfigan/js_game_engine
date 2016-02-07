12e3c896246487524895904089408d89799a8e60
Ambrose McJunkin <jerorfigan@gmail.com>
2016-02-07 16:56:46 -0700

    Change game engine structure to adhere to the following tennet: only basic objects should be rendered and only manager objects should be updated

--- Managers/GameManager.js
+++ Managers/GameManager.js
@@ -7,17 +7,13 @@ if(!window.sft) window.sft = {};
 	};
 
 	GameManager.prototype.draw = function(){
-		var renderList = window.sft.objMgr.getAllWithClassOrAttr("", "renderAttr");
+		var renderList = window.sft.objMgr.getAllWithClassOrAttr("basic", "renderAttr");
 		renderList.forEach(function(obj){ obj.renderAttr.render(); });
 	};
 
 	GameManager.prototype.update = function(){
-		/* Update manager objects before basic objects, as managers will change the update behavior of basic objects */
 		var managerUpdateList = window.sft.objMgr.getAllWithClassOrAttr("manager", "updateAttr");
 		managerUpdateList.forEach(function(obj){ obj.updateAttr.update(); });
-
-		var basicObjUpdateList = window.sft.objMgr.getAllWithClassOrAttr("basic", "updateAttr");
-		basicObjUpdateList.forEach(function(obj){ obj.updateAttr.update(); });
 	};
 
 	window.sft.GameManager = GameManager;
